385dae6f5bd09a6ffade73ce00a15332fa341bc2
bitbucket-Roshitgit <roshitrajan@gmail.com>
2018-07-09 20:17:47 +0530

    JWT in intranet ENV

---
+++ DotNetCoreApp.UI/Controllers/WindowsAuthenticationController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreApp.UI.Controllers
+{
+    // JWT in Intranet Environment.
+    //https://stackoverflow.com/questions/27217424/use-windows-authentication-with-oauth-2-0
+
+    // [RoutePrefix("token/windows")]
+    // public class WindowsAuthenticationController: ApiController
+    // {
+    //     [Authorize]
+    //     [HttpPost]
+    //     [Route("{client_id}"]
+    //     public async Task<IHttpActionResult> CreateTokenForWindowsIdentity(string client_id)
+    //     {
+    //         var user = User as ClaimsPrincipal;
+    //         if (user == null) return Unauthorized(); //401
+    //         var claims = //generate claims based on the User.Identity.Name...
+    //         var identity = new ClaimsIdentity("JWT");
+    //         identity.AddClaims(claims);
+
+    //         //manually create JWT using whatever method you prefer,
+    //         //I used something inspired from http://bitoftech.net/2015/02/16/implement-oauth-json-web-tokens-authentication-in-asp-net-web-api-and-identity-2/
+    //     }
+    // }
+}
